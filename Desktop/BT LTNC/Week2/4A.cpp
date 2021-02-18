0f3dae005aaebf152a8ba7a9d605e869b3768191
Nguyễn Tấn Minh <78575962+20020081@users.noreply.github.com>
2021-02-18 16:56:45 +0700

    Update 4A.cpp

--- Desktop/BT LTNC/Week2/4A.cpp
+++ Desktop/BT LTNC/Week2/4A.cpp
@@ -33,14 +33,57 @@ int daysinmonth(int m, int y){
 }
 
 int tongngay(int d, int m, int y){
-
+    int tongsongay = 0;
+    for(int i = 1; i < m; i++){
+        tongsongay += daysinmonth(i, y);
+    }
+    tongsongay += d;
+    return tongsongay;
 }
 int check(int d, int m, int y, int k){
-    if(d + k >= 1 && d + k <= daysinmonth(m))
+    if(d + k >= 1 && d + k < daysinmonth(m))
         return 1;
-    else if(d + k <= 0)
+    else if(tongngay + k > 0 && tongngay + k < daysinyear(y))
+        return 2;
+    else return 3;
 }
-
+void solve1(int &d, int k){
+    d += k;
+}
+void solve2(int &d, int &m, int &y, int k){
+    int after = tongngay(d, m, y) + k;
+    int t = 0;
+    for(int i = 1; i <= after/30; i++){
+        t += daysinmonth(i); 
+    }
+    d = after - t;
+    m = after/30 + 1;
+}
+void solve3(int &d, int &m, int &y, int k){
+    int after = tongngay(d, m, y) + k;
+    if(after > 0){
+        do{
+            if(Leapyear(y))
+                after -= 366;
+            else after -= 365;
+        }
+        while(after >= 365){
+            y++;
+            if(Leapyear(y))
+                after -= 366;
+            else after -= 365;
+        }
+        m = after/30 + 1;
+        int t = 0;
+        for(int i = 1; i < m; i++){
+            t += daysinmonth(i, y)
+        }
+        d = after - t;
+    }
+    else{ // lui nam
+        
+    }
+}
 int main(){
     int d, m, y;
     cin >> d >> m >> y;
@@ -48,15 +91,16 @@ int main(){
     int k; cin >> k;
 
     while(k != 0){
-        if(check(d, m, y) == 1){
-            sumdayinmonth(d, k);
-            cout << d << "-" << m << "-" << y;
+        if(check(d, m, y, k) == 1){
+            solve1(d, k);
         }
-        else if(check(d, m, y) == 2){
-            sumdayoutmonth(d, m, k);
-            cout << d << "-" << m << "-" << y;
+        else if(check(d, m, y, k) == 2){
+            solve2(d, m, y, k);
         }
-        else if()
+        else if(check(d, m, y, k) == 3){
+            solve3(d, m, y, k);
+        }
+            cout << d << "-" << m << "-" << y << endl;
     }
 
     return 0;

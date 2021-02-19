26e2fd05735d3c3cd005a2c9bc144601d72c0568
Nguyễn Tấn Minh <78575962+20020081@users.noreply.github.com>
2021-02-19 23:51:10 +0700

    Update 4A.cpp

--- Desktop/BT LTNC/Week2/4A.cpp
+++ Desktop/BT LTNC/Week2/4A.cpp
@@ -32,41 +32,63 @@ int daysinmonth(int m, int y){
     }
 }
 
-int tongngay(int d, int m, int y){
+int dayafterinyear(int d, int m, int y, int k){
     int tongsongay = 0;
     for(int i = 1; i < m; i++){
         tongsongay += daysinmonth(i, y);
     }
     tongsongay += d;
-    return tongsongay;
+    return tongsongay + k;
 }
 int check(int d, int m, int y, int k){
-    if(d + k >= 1 && d + k < daysinmonth(m))
+    if(d + k >= 1 && d + k <= daysinmonth(m, y))
         return 1;
-    else if(tongngay + k > 0 && tongngay + k < daysinyear(y))
+    else if(((d + k <= 0) || (d + k > daysinmonth(m, y))) && dayafterinyear(d, m, y, k) + k > 0 && dayafterinyear(d, m, y, k) + k < daysinyear(y))
         return 2;
     else return 3;
+}
+int daystomonth(int m, int y){
+    int days = 0;
+    for(int i = 1; i <= m; i++){
+        days += daysinmonth(i, y);
+    }
+    return days;
 }
 void solve1(int &d, int k){
     d += k;
 }
 void solve2(int &d, int &m, int &y, int k){
-    int after = tongngay(d, m, y) + k;
-    int t = 0;
-    for(int i = 1; i <= after/30; i++){
-        t += daysinmonth(i); 
+    int after = dayafterinyear(d, m, y, k);
+    for(int i = 1; i <= 11; i++){
+        if(after <= 31){
+            m = 1;
+            break;
+        }
+        else{
+            if(after > daystomonth(i, y) && after <= daystomonth(i+1, y)){
+                m = i+1;
+                break;
+            }
+        }
     }
-    d = after - t;
-    m = after/30 + 1;
+    if(m == 1){
+        d = after;
+    }else{
+        int t = 0;
+        for(int i = 1; i < m; i++){
+            t += daysinmonth(i, y);
+        }
+        d = after - t;
+    }
 }
 void solve3(int &d, int &m, int &y, int k){
-    int after = tongngay(d, m, y) + k;
+    int after = dayafterinyear(d, m, y, k);
     if(after > 0){
-        do{
-            if(Leapyear(y))
-                after -= 366;
-            else after -= 365;
-        }
+        // chia ngày đến hết năm hiện tại
+        if(Leapyear(y))
+            after -= 366;
+        else after -= 365;
+        // chia ngày cho các năm sau
         while(after >= 365){
             y++;
             if(Leapyear(y))
@@ -76,21 +98,31 @@ void solve3(int &d, int &m, int &y, int k){
         m = after/30 + 1;
         int t = 0;
         for(int i = 1; i < m; i++){
-            t += daysinmonth(i, y)
+            t += daysinmonth(i, y);
         }
         d = after - t;
     }
     else{ // lui nam
-        
+
     }
 }
+void resultout(int d, int m, int y){
+    if(d < 10) cout << 0 << d;
+    else cout << d;
+    cout << "-";
+    if(m < 10) cout << 0 << m;
+    else cout << m;
+    cout << "-";
+    cout << y << endl;
+}
 int main(){
-    int d, m, y;
-    cin >> d >> m >> y;
-    m *= -1; y *= -1;
-    int k; cin >> k;
+    int d, m, y, k;
+    char c;
+    cin >> d >> c >> m >> c >> y;
 
-    while(k != 0){
+    while(cin >> k){
+        if(k == 0) break;
+
         if(check(d, m, y, k) == 1){
             solve1(d, k);
         }
@@ -100,7 +132,7 @@ int main(){
         else if(check(d, m, y, k) == 3){
             solve3(d, m, y, k);
         }
-            cout << d << "-" << m << "-" << y << endl;
+        resultout(d, m, y);
     }
 
     return 0;

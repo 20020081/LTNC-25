88047522330005f61574fac22f20d78cb494049a
Nguyễn Tấn Minh <78575962+20020081@users.noreply.github.com>
2021-02-27 16:41:53 +0700

    Update 4C.cpp

--- Desktop/BT LTNC/Week3/4C.cpp
+++ Desktop/BT LTNC/Week3/4C.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int detect_mine(char f[10][10], int i, int j){
+int detect_mine(char f[][10], int i, int j){
     int mine = 0;
     if(f[i][j+1] == '*') mine++;
     if(f[i][j-1] == '*') mine++;
@@ -19,18 +19,18 @@ int detect_mine(char f[10][10], int i, int j){
 int main(){
     int row, col;
     cin >> row >> col;
-    char f[col][row];
+    char f[10][10];
     for(int i = 0; i < row; i++){
         for(int j = 0; j < col; j++){
-            cin >> f[row][col];
+            cin >> f[i][j];
         }
     }
 
     for(int i = 0; i < row; i++){
         for(int j = 0; j < col; j++){
-            if(f[i][j] == 42) // k chay cau lenh if
+            if(f[i][j] == 42)
                 cout << "*" << " ";
-            else cout << 1 << " ";
+            else cout <<  detect_mine(f, i, j) << " ";
         }
         cout << endl;
     }

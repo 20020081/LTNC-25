54024c8508fc520825a43aa3f904e385362d8c83
Nguyễn Tấn Minh <78575962+20020081@users.noreply.github.com>
2021-03-04 20:53:08 +0700

    Update 8C.cpp

--- Desktop/BT LTNC/Week3/8C.cpp
+++ Desktop/BT LTNC/Week3/8C.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 
 int main(){
-    int n; cin >> n;
-    int f[n];
+    long long n, i, j, t; cin >> n;
+    long long f[n];
     for(int i = 0; i < n; i++){
         cin >> f[i];
     }
 
-    sort(f, f + n);
-    int ans = 0;
-    for(int i = 0; i < n-1; i++){
-        ans += f[i];
+    for(i = 0; i < n; i++){
+        for(j= 0; j < n - i - 1; j++){
+            if(f[i] > f[i+1]){
+                t = f[i];
+                f[i] = f[i+1];
+                f[i+1] = t;
+            }
+        }
+    }
+
+    long long ans = 0;
+    for(i = 0; i < n-1; i++){
+        for(j = 0; j < i; j++)
+            ans += f[i];
     }
 
     cout << ans;

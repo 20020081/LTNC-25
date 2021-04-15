471916c3dd87c873bf2ee3ba402fcd9fe29dca6e
Nguyễn Tấn Minh <78575962+20020081@users.noreply.github.com>
2021-04-15 23:09:01 +0700

    update 1a

--- Desktop/BT LTNC/Week9/1A.cpp
+++ Desktop/BT LTNC/Week9/1A.cpp
@@ -1,19 +1,11 @@
 #include<iostream>
 #include<cstring>
 using namespace std;
-//
-//● trong hàm concat, bạn có thể dùng hàm trong thư viện <cstring>, chẳng hạn strcpy() hay
-//strncpy() để sao chép xâu, strcat hay strncat để nối xâu. Xem chi tiết và ví dụ tại
-//http://www.cplusplus.com/reference/cstring/.
-//● Ý tưởng thuật toán: (1) tính ra kích thước của xâu kết quả, (2) cấp phát động mảng kết
-//quả với kích thước vừa tính, (3) chép xâu thứ nhất vào đầu mảng động, (4) chép/nối
-char* concat(const char* s1, const char* s2){
-    int nf = strlen(s1), ns = strlen(s2);
-//    cout << s1 << endl;
-    cout << nf << " " << ns;
 
-    int n = nf + ns, i = 0;
+char* concat(const char* s1, const char* s2){
+    int n = strlen(s1) + strlen(s2);
     char* str = new char [n];
+    *str = '\0';
 
     strcat(str, s1);
     strcat(str, s2);
@@ -22,10 +14,9 @@ char* concat(const char* s1, const char* s2){
 }
 
 int main(){
-     char c1 = 'Hello';
-     char c2 = 'Word';
-//    cout << &c1 << ' ' << &c2 << endl;
-    concat(&c1, &c2);
+    char c1[] = "Hello";
+    char c2[] = "Word";
+    cout << concat(c1, c2);
 
     return 0;
 }
